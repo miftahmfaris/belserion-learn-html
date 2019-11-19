ad9ad4a29521e724442cf275e04dcc5f865ca429
miftah faris <himeprojects@gmail.com>
2019-11-19 11:01:21 +0700

    update signup component

--- learn-react/src/components/SignUp.js
+++ learn-react/src/components/SignUp.js
@@ -2,110 +2,191 @@ import React from "react";
 import { withRouter } from "react-router-dom";
 import { Formik, ErrorMessage } from "formik";
 import { validationForm } from "../validate";
+import Avatar from "@material-ui/core/Avatar";
+import Button from "@material-ui/core/Button";
+import CssBaseline from "@material-ui/core/CssBaseline";
+import TextField from "@material-ui/core/TextField";
+import Link from "@material-ui/core/Link";
+import Grid from "@material-ui/core/Grid";
+import LockOutlinedIcon from "@material-ui/icons/LockOutlined";
+import Typography from "@material-ui/core/Typography";
+import { makeStyles } from "@material-ui/core/styles";
+import Container from "@material-ui/core/Container";
 
-function SignUp() {
+const useStyles = makeStyles(theme => ({
+    "@global": {
+        body: {
+            backgroundColor: theme.palette.common.white
+        }
+    },
+    paper: {
+        marginTop: theme.spacing(8),
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center"
+    },
+    avatar: {
+        margin: theme.spacing(1),
+        backgroundColor: theme.palette.secondary.main
+    },
+    form: {
+        width: "100%", // Fix IE 11 issue.
+        marginTop: theme.spacing(3)
+    },
+    submit: {
+        margin: theme.spacing(3, 0, 2)
+    }
+}));
+
+function SignUp(props) {
+    const classes = useStyles();
+
     return (
-        <div>
-            <Formik
-                initialValues={{
-                    email: "",
-                    password: ""
-                }}
-                validate={validationForm}
-                onSubmit={(values, { setSubmitting }) => {
-                    localStorage.setItem("user", JSON.stringify(values));
-                    this.props.history.push("/signin");
-                }}
-            >
-                {({
-                    values,
-                    handleChange,
-                    handleBlur,
-                    handleSubmit,
-                    isSubmitting
-                }) => (
-                    <form onSubmit={handleSubmit}>
-                        <fieldset>
-                            <div>
-                                <label htmlFor="firstName">First Name:</label>
-                                <input
-                                    type="text"
-                                    name="firstName"
-                                    onChange={handleChange}
-                                    onBlur={handleBlur}
-                                    value={values.firstName}
-                                />
-                                <p
-                                    style={{
-                                        color: "red",
-                                        fontStyle: "italic"
-                                    }}
-                                >
-                                    <ErrorMessage name="firstName" />
-                                </p>
-                            </div>
-                            <div>
-                                <label htmlFor="lastName">Last Name:</label>
-                                <input
-                                    type="text"
-                                    name="lastName"
-                                    onChange={handleChange}
-                                    onBlur={handleBlur}
-                                    value={values.lastName}
-                                />
-                                <p
-                                    style={{
-                                        color: "red",
-                                        fontStyle: "italic"
-                                    }}
-                                >
-                                    <ErrorMessage name="lastName" />
-                                </p>
-                            </div>
-                            <div>
-                                <label htmlFor="email">Email:</label>
-                                <input
-                                    type="email"
-                                    name="email"
-                                    onChange={handleChange}
-                                    onBlur={handleBlur}
-                                    value={values.email}
-                                />
-                                <p
-                                    style={{
-                                        color: "red",
-                                        fontStyle: "italic"
-                                    }}
-                                >
-                                    <ErrorMessage name="email" />
-                                </p>
-                            </div>
-                            <div>
-                                <label htmlFor="password">Password:</label>
-                                <input
-                                    type="password"
-                                    name="password"
-                                    onChange={handleChange}
-                                    onBlur={handleBlur}
-                                    value={values.password}
-                                />
-                                <p
-                                    style={{
-                                        color: "red",
-                                        fontStyle: "italic"
-                                    }}
-                                >
-                                    <ErrorMessage name="password" />
-                                </p>
-                            </div>
-                            <button type="submit" disabled={isSubmitting}>
-                                Register
-                            </button>
-                        </fieldset>
-                    </form>
-                )}
-            </Formik>
-        </div>
+        <Container component="main" maxWidth="xs">
+            <CssBaseline />
+            <div className={classes.paper}>
+                <Avatar className={classes.avatar}>
+                    <LockOutlinedIcon />
+                </Avatar>
+                <Typography component="h1" variant="h5">
+                    Sign up
+                </Typography>
+                <Formik
+                    initialValues={{
+                        email: "",
+                        password: ""
+                    }}
+                    validate={validationForm}
+                    onSubmit={(values, { setSubmitting }) => {
+                        localStorage.setItem("user", JSON.stringify(values));
+                        props.history.push("/signin");
+                    }}
+                >
+                    {({
+                        values,
+                        handleChange,
+                        handleBlur,
+                        handleSubmit,
+                        isSubmitting
+                    }) => (
+                        <form
+                            className={classes.form}
+                            noValidate
+                            onSubmit={handleSubmit}
+                        >
+                            <Grid container spacing={2}>
+                                <Grid item xs={12} sm={6}>
+                                    <TextField
+                                        autoComplete="fname"
+                                        name="firstName"
+                                        variant="outlined"
+                                        required
+                                        fullWidth
+                                        id="firstName"
+                                        label="First Name"
+                                        autoFocus
+                                        onChange={handleChange}
+                                        onBlur={handleBlur}
+                                        defaultValue={values.firstName}
+                                    />
+                                    <p
+                                        style={{
+                                            color: "red",
+                                            fontStyle: "italic"
+                                        }}
+                                    >
+                                        <ErrorMessage name="firstName" />
+                                    </p>
+                                </Grid>
+                                <Grid item xs={12} sm={6}>
+                                    <TextField
+                                        variant="outlined"
+                                        required
+                                        fullWidth
+                                        id="lastName"
+                                        label="Last Name"
+                                        name="lastName"
+                                        autoComplete="lname"
+                                        onChange={handleChange}
+                                        onBlur={handleBlur}
+                                        defaultValue={values.lastName}
+                                    />
+                                    <p
+                                        style={{
+                                            color: "red",
+                                            fontStyle: "italic"
+                                        }}
+                                    >
+                                        <ErrorMessage name="lastName" />
+                                    </p>
+                                </Grid>
+                                <Grid item xs={12}>
+                                    <TextField
+                                        variant="outlined"
+                                        required
+                                        fullWidth
+                                        id="email"
+                                        label="Email Address"
+                                        name="email"
+                                        autoComplete="email"
+                                        onChange={handleChange}
+                                        onBlur={handleBlur}
+                                        defaultValue={values.email}
+                                    />
+                                    <p
+                                        style={{
+                                            color: "red",
+                                            fontStyle: "italic"
+                                        }}
+                                    >
+                                        <ErrorMessage name="email" />
+                                    </p>
+                                </Grid>
+                                <Grid item xs={12}>
+                                    <TextField
+                                        variant="outlined"
+                                        required
+                                        fullWidth
+                                        name="password"
+                                        label="Password"
+                                        type="password"
+                                        id="password"
+                                        autoComplete="current-password"
+                                        onChange={handleChange}
+                                        onBlur={handleBlur}
+                                        defaultValue={values.password}
+                                    />
+                                    <p
+                                        style={{
+                                            color: "red",
+                                            fontStyle: "italic"
+                                        }}
+                                    >
+                                        <ErrorMessage name="password" />
+                                    </p>
+                                </Grid>
+                            </Grid>
+                            <Button
+                                type="submit"
+                                fullWidth
+                                variant="contained"
+                                color="primary"
+                                className={classes.submit}
+                            >
+                                Sign Up
+                            </Button>
+                            <Grid container justify="flex-end">
+                                <Grid item>
+                                    <Link href="#" variant="body2">
+                                        Already have an account? Sign in
+                                    </Link>
+                                </Grid>
+                            </Grid>
+                        </form>
+                    )}
+                </Formik>
+            </div>
+        </Container>
     );
 }
 
