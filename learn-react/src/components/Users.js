8fc6c28f143d4584a8adfb08fe5d75eda270b2ce
miftah faris <himeprojects@gmail.com>
2019-11-20 16:49:40 +0700

    add user

--- learn-react/src/components/Users.js
+++ learn-react/src/components/Users.js
@@ -1,13 +1,79 @@
-import React from "react";
+import React, { Component } from "react";
+import axios from "axios";
 import { Link } from "react-router-dom";
+import Card from "@material-ui/core/Card";
+import CardActions from "@material-ui/core/CardActions";
+import CardContent from "@material-ui/core/CardContent";
+import Button from "@material-ui/core/Button";
+import Typography from "@material-ui/core/Typography";
+export default class About extends Component {
+    constructor(props) {
+        super(props);
 
-export default function Users() {
-    const name = "faris";
-    return (
-        <div>
-            <p>
-                <Link to={`/users/${name}`}>Ini adalah halaman user</Link>
-            </p>
-        </div>
-    );
+        this.state = {
+            data: []
+        };
+    }
+
+    componentDidMount = () => {
+        axios
+            .get("https://jsonplaceholder.typicode.com/users")
+            .then(response => {
+                this.setState({ data: response.data });
+            })
+            .catch(error => {
+                console.log(error);
+            });
+    };
+
+    render() {
+        return (
+            <div
+                style={{
+                    display: "flex",
+                    flexWrap: "wrap",
+                    justifyContent: "space-around"
+                }}
+            >
+                {this.state.data.length > 0 &&
+                    this.state.data.map((item, key) => {
+                        return (
+                            <Card
+                                style={{ maxWidth: "300px", margin: "10px" }}
+                                key={key}
+                            >
+                                <CardContent>
+                                    <Typography color="textSecondary">
+                                        {item.email}
+                                    </Typography>
+                                    <Typography variant="body1" component="p">
+                                        Hello my name is {item.name}
+                                    </Typography>
+                                    <br />
+                                    <Typography
+                                        variant="subtitle2"
+                                        component="p"
+                                    >
+                                        Phone: {item.phone}
+                                    </Typography>
+                                    <Typography
+                                        variant="subtitle2"
+                                        component="p"
+                                    >
+                                        Website: {item.website}
+                                    </Typography>
+                                </CardContent>
+                                <CardActions>
+                                    <Button size="small">
+                                        <Link to={`/users/${item.id}`}>
+                                            Learn More
+                                        </Link>
+                                    </Button>
+                                </CardActions>
+                            </Card>
+                        );
+                    })}
+            </div>
+        );
+    }
 }
